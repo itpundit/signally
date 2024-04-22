612508bdc646230cefd87f924bfc6ffebc535d43
unknown <dev-env@itpundit.org>
2024-04-22 18:39:28 +0530

    updated

--- src/pages/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next/types';
-
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'A server lives here' });
-}
