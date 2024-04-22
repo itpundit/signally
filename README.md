69a7725df78b5cd4941164f39b066be60d5e6c7f
unknown <dev-env@itpundit.org>
2024-04-22 18:31:00 +0530

    signally up

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# signally
-signally
+# Signally
--- _next/static/chunks/4369.e75e92489e734a31.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4369],{94369:function(e,u,s){s.r(u);var a=s(24038);u.default=a.H}}]);
